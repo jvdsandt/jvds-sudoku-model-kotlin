85f926440db7174c173444ae04308fb1a4cf8b0b
Jan van de Sandt <jvdsandt@gmail.com>
2017-04-19 18:25:14 +0200

    Data classes tests

---
+++ src/test/kotlin/com/cloudctrl/sudoku/model/SudokuDataClassesTest.kt
@@ -0,0 +1,31 @@
+package com.cloudctrl.sudoku.model
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class SudokuDataClassesTest {
+
+    @Test fun testCell() {
+        val cell = SudokuCell(1, 2)
+        assertEquals(SudokuCell(1, 2), cell)
+        assertEquals(SudokuCell(1, 2).hashCode(), cell.hashCode())
+        assertNotEquals(SudokuCell(2, 3), cell)
+    }
+
+    @Test fun testDestructing() {
+        val (x, y) = SudokuCell(4, 5)
+        assertEquals(4, x)
+        assertEquals(5, y)
+    }
+
+    @Test fun testCopy() {
+        val cell = SudokuCell(1, 2)
+        val copy1 = cell.copy()
+        assertEquals(cell, copy1)
+        val copy2 = cell.copy(x = 99)
+        assertNotEquals(cell, copy2)
+        assertEquals(99, copy2.x)
+        assertEquals(2, copy2.y)
+    }
+}
