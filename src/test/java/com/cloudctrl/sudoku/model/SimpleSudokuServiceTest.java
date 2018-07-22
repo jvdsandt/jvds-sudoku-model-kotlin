2d5238ca5da1ff9cf8fa033eca77999803acfc1a
Jan van de Sandt <jvdsandt@gmail.com>
2018-07-22 15:38:03 +0200

    Added Java service class

--- src/test/java/com/cloudctrl/sudoku/model/SimpleSudokuServiceTest.java
+++ src/test/java/com/cloudctrl/sudoku/model/SimpleSudokuServiceTest.java
@@ -23,12 +23,12 @@ public class SimpleSudokuServiceTest {
     }
 
     @Test
-    public void testBasicsNextMove() {
-
-        SudokuMove move = service.getNextMove("003078000768152003010000500000000009096703120800000000004000030900835764000640200");
-
-        assertEquals(new SudokuCell(2, 8), move.getCell());
-        assertEquals(2, move.getValue());
-        assertEquals(SudokuMoveReason.ONLY_OPTION, move.getReason());
+    public void testInvalidSolve() {
+        try {
+            service.solve("xyz");
+            fail();
+        } catch (IllegalArgumentException ex) {
+            assertEquals("Not enough numbers provided", ex.getMessage());
+        }
     }
 }
